7072e9c9d64da7620a6d83380a6d488d83f59315
Баркалов  Владимир <kot.yoyga@gmail.com>
2026-09-01 08:43:54 +0300

    Export high-level draft coach service

--- dota_coach/__init__.py
+++ dota_coach/__init__.py
@@ -1,6 +1,15 @@
 """Dota Coach MVP package."""
 
 from .data import DotaData, Hero
-from .engine import Pick, recommend, build_strategy
+from .engine import Pick, build_strategy, recommend
+from .service import DraftResult, coach_draft
 
-__all__ = ["DotaData", "Hero", "Pick", "recommend", "build_strategy"]
+__all__ = [
+    "DotaData",
+    "Hero",
+    "Pick",
+    "DraftResult",
+    "recommend",
+    "build_strategy",
+    "coach_draft",
+]
