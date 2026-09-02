4744a5f02fa852001ee593930603fa4b5bb22b0d
Баркалов  Владимир <kot.yoyga@gmail.com>
2026-09-02 08:57:34 +0300

    Export draft recognition MVP API

--- dota_coach/__init__.py
+++ dota_coach/__init__.py
@@ -1,7 +1,10 @@
 """Dota Coach MVP package."""
 
 from .data import DotaData, Hero
+from .draft_layout import DraftLayout, NormalizedRect, SlotRegion
+from .draft_recognition import DraftRecognition, SlotRecognition, recognize_draft_slots
 from .engine import Pick, build_strategy, recommend
+from .portrait import PortraitIndex, PortraitMatch
 from .service import DraftResult, coach_draft
 
 __all__ = [
@@ -9,7 +12,15 @@ __all__ = [
     "Hero",
     "Pick",
     "DraftResult",
+    "DraftLayout",
+    "NormalizedRect",
+    "SlotRegion",
+    "PortraitIndex",
+    "PortraitMatch",
+    "DraftRecognition",
+    "SlotRecognition",
     "recommend",
     "build_strategy",
     "coach_draft",
+    "recognize_draft_slots",
 ]
