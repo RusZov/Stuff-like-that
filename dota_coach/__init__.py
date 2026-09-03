40631d198336f9998ac2ad235257cbdef1ba6ebd
Баркалов  Владимир <kot.yoyga@gmail.com>
2026-09-03 08:46:42 +0300

    Export draft validation and recognized coaching APIs

--- dota_coach/__init__.py
+++ dota_coach/__init__.py
@@ -2,7 +2,20 @@
 
 from .data import DotaData, Hero
 from .draft_layout import DraftLayout, NormalizedRect, SlotRegion
+from .draft_mvp import (
+    RecognizedCoachResult,
+    RecognizedDraftInput,
+    coach_recognized_draft,
+    recognition_to_draft_input,
+)
 from .draft_recognition import DraftRecognition, SlotRecognition, recognize_draft_slots
+from .draft_validation import (
+    AnchorEvidence,
+    DraftAnchorProfile,
+    DraftFrameValidation,
+    calibrate_anchor_profile,
+    validate_draft_frame,
+)
 from .engine import Pick, build_strategy, recommend
 from .portrait import PortraitIndex, PortraitMatch
 from .service import DraftResult, coach_draft
@@ -19,8 +32,17 @@ __all__ = [
     "PortraitMatch",
     "DraftRecognition",
     "SlotRecognition",
+    "DraftAnchorProfile",
+    "AnchorEvidence",
+    "DraftFrameValidation",
+    "RecognizedDraftInput",
+    "RecognizedCoachResult",
     "recommend",
     "build_strategy",
     "coach_draft",
     "recognize_draft_slots",
+    "calibrate_anchor_profile",
+    "validate_draft_frame",
+    "recognition_to_draft_input",
+    "coach_recognized_draft",
 ]
